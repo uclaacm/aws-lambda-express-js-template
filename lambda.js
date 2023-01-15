74903d577a075f6d2134266ab67744879bc03866
Johanna Liu <jojo.cj.liui@gmail.com>
2023-01-14 23:14:49 -0800

    changed run function in claudia-deploy to deploy express app

--- lambda.js
+++ lambda.js
@@ -1,4 +1,14 @@
-exports.handler = function (event, context) {
-  console.log(event);
-  context.succeed("Yo what's up: ", event.name);
-};
+'use strict'
+const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./app')
+const binaryMimeTypes = [
+	'application/octet-stream',
+	'font/eot',
+	'font/opentype',
+	'font/otf',
+	'image/jpeg',
+	'image/png',
+	'image/svg+xml'
+]
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
